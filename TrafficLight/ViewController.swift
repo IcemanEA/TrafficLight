3064b334aba892bacfd7ad0da3cccc162056ece7
Egor Ledkov <egor@ledkov.org>
2022-06-17 01:21:16 +0300

    Setup UI

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    
+    @IBOutlet var lightStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func nextButtonTapped() {
+        redLightView.alpha = 1
+        
+    }
+    
 }
 
